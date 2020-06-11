4a91503320e93674fbc3e8eaa0283c5f9e52c6a8
dranzerAvi <aviralhtc2015@gmail.com>
2020-06-12 00:48:37 +0530

    Dropdown button rebuilt due to error🤘

---
+++ lib/HomePageScreens/allCitiesScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AllCitiesScreen extends StatefulWidget {
+  @override
+  _AllCitiesScreenState createState() => _AllCitiesScreenState();
+}
+
+class _AllCitiesScreenState extends State<AllCitiesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text('data')),
+    );
+  }
+}
---
+++ lib/HomePageScreens/jaipurScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class JaipurScreen extends StatefulWidget {
+  @override
+  _JaipurScreenState createState() => _JaipurScreenState();
+}
+
+class _JaipurScreenState extends State<JaipurScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
