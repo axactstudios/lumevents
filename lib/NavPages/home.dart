628e2705c78280ef69f836f435e2f9a22f8e8b8f
scorchy <vkumarsaraswat@gmail.com>
2020-06-12 21:24:59 +0530

    Backend configured, searchable dialog box.

--- lib/NavPages/home.dart
+++ lib/NavPages/home.dart
@@ -4,6 +4,7 @@ import 'package:lumevents/HomePageScreens/delhiNCRScreen.dart';
 import 'package:lumevents/HomePageScreens/jaipurScreen.dart';
 import 'package:lumevents/HomePageScreens/kolkataScreen.dart';
 import 'package:lumevents/HomePageScreens/udaipurScreen.dart';
+import 'package:searchable_dropdown/searchable_dropdown.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -27,46 +28,47 @@ class _HomePageState extends State<HomePage> {
     return SafeArea(
       child: Scaffold(
           appBar: AppBar(
-
+            automaticallyImplyLeading: false,
             backgroundColor: Color(0xFFFF4B8F),
-
-
-            actions: <Widget>[
-              Theme(
-                child: DropdownButtonHideUnderline(
-                  child: DropdownButton(
-                    items: _cities.map((String dropDownStringItem) {
-                      return DropdownMenuItem<String>(
-                        child: Text(
-                          dropDownStringItem,
-                          style: TextStyle(
-                              color: Colors.white, fontFamily: 'sf_pro'),
-                        ),
-                        value: dropDownStringItem,
-                      );
-                    }).toList(),
-                    onChanged: (String newValueSelected) {
-                      setState(() {
-                        _currentItemSelected = newValueSelected;
-                        if (_currentItemSelected == _cities[0]) {
-                          _currentScreenToShow = AllCitiesScreen();
-                        } else if (_currentItemSelected == _cities[1]) {
-                          _currentScreenToShow = DelhiNCRScreen();
-                        } else if (_currentItemSelected == _cities[2]) {
-                          _currentScreenToShow = JaipurScreen();
-                        } else if (_currentItemSelected == _cities[3]) {
-                          _currentScreenToShow = KolkataScreen();
-                        } else if (_currentItemSelected == _cities[4]) {
-                          _currentScreenToShow = UdaipurScreen();
-                        }
-                      });
-                    },
-                    value: _currentItemSelected,
-                  ),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SearchableDropdown(
+                  menuBackgroundColor: Color(0xFFFF4B8F),
+                  iconEnabledColor: Colors.white,
+                  iconDisabledColor: Colors.white,
+                  items: _cities.map((String dropDownStringItem) {
+                    return DropdownMenuItem<String>(
+                      child: Text(
+                        dropDownStringItem,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'sf_pro',
+                            fontWeight: FontWeight.bold),
+                      ),
+                      value: dropDownStringItem,
+                    );
+                  }).toList(),
+                  onChanged: (String newValueSelected) {
+                    setState(() {
+                      _currentItemSelected = newValueSelected;
+                      if (_currentItemSelected == _cities[0]) {
+                        _currentScreenToShow = AllCitiesScreen();
+                      } else if (_currentItemSelected == _cities[1]) {
+                        _currentScreenToShow = DelhiNCRScreen();
+                      } else if (_currentItemSelected == _cities[2]) {
+                        _currentScreenToShow = JaipurScreen();
+                      } else if (_currentItemSelected == _cities[3]) {
+                        _currentScreenToShow = KolkataScreen();
+                      } else if (_currentItemSelected == _cities[4]) {
+                        _currentScreenToShow = UdaipurScreen();
+                      }
+                    });
+                  },
+                  value: _currentItemSelected,
                 ),
-                data: ThemeData.dark(),
-              ),
-            ],
+              ],
+            ),
           ),
           body: _currentScreenToShow),
     );
